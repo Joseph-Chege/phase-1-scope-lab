122af9635a4a9ba65be537fb94b5c827be605162
Joseph-Chege <mjchege23@gmail.com>
2024-04-01 14:50:09 +0300

    Created functions to understand scope

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName =  "bob"
+
+function upperCaseCustomerName(){
+    if (customerName !== undefined){
+        return customerName = customerName.toUpperCase();
+
+    }      
+
+}  
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob"
+}
+    
+function  overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "suzy"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Pep Guardiola"
+}
